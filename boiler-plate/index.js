f664e865da130ddabb6189ea03a642e2c73f0a45
yooji0415 <gch05406@gmail.com>
2022-08-22 19:09:45 +0900

    feat: boiler-plate backend

---
+++ boiler-plate/index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // 최신버전 mongoose 에서 디폴트 설정으로 만들어뒀다.
+    //   useCreateIndex: true,
+    //   useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", (req, res) => {
+  // 회원 가입시 필요한 정보들을 client 에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err: err.message });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+    // 요청한 이메일이 데이터베이스에 있다면 비밀번호가 동일한지 확인한다.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+
+      // 비밀번호가 맞다면 토큰을 생성한다.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
